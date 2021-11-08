2d4d62246d210eff20ec91ca28290cc35c0d0a5b
sarah <sarakane17@gmail.com>
2021-11-08 12:29:06 +0300

    practice 1

---
+++ 1- Simple Pig Latin/index.js
@@ -0,0 +1,20 @@
+// Instructions
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
+// Leave punctuation marks untouched.
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+// Solution
+str = process.argv.slice(2)[0];
+pigIt(str);
+function pigIt(str){
+    //split the string by space
+    str_arr = str.split(" "); 
+    final = "";
+    // get first letter and add ay, then remove the first letter and append to variable.
+    for(i=0; i< str_arr.length ;i++){
+        final += (str_arr[i]+str_arr[i].charAt(0)+"ay ").replace(str_arr[i].charAt(0), "");
+    }
+    console.log(final.trim());
+}
