2b0fbf52ea727b210aafe5984678abef761b9197
sarah <sarakane17@gmail.com>
2021-11-08 12:39:12 +0300

    exclamation factored in:

--- 1- Simple Pig Latin/index.js
+++ 1- Simple Pig Latin/index.js
@@ -5,7 +5,7 @@
 // pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 // pigIt('Hello world !');     // elloHay orldway !
 
-// Solution
+// Solution(5kyu)
 str = process.argv.slice(2)[0];
 pigIt(str);
 function pigIt(str){
@@ -14,7 +14,12 @@ function pigIt(str){
     final = "";
     // get first letter and add ay, then remove the first letter and append to variable.
     for(i=0; i< str_arr.length ;i++){
-        final += (str_arr[i]+str_arr[i].charAt(0)+"ay ").replace(str_arr[i].charAt(0), "");
+        if(str_arr[i] != "!" && str_arr[i] != "?"){
+            final += (str_arr[i]+str_arr[i].charAt(0)+"ay ").replace(str_arr[i].charAt(0), "");
+        }
+        else{
+            final += str_arr[i];
+        }
     }
     console.log(final.trim());
 }
